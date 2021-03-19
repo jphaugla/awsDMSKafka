d494a4c1c2f227fd9a3e228e978b1711dfe3f68e
Haugland <jphaugla@3c22fba09f76.ant.amazon.com>
2021-03-19 08:16:21 -0500

    mapping change for partitioning

---
+++ template/modifyReplicationTask2.sh
@@ -0,0 +1,1 @@
+aws dms modify-replication-task --replication-task-arn arn:aws:dms:us-east-1:569119288395:task:YTOIBGJJ2DFVLWXVEZVHSDHPJFOR742MLB2BXKY --table-mappings file://table-mapping.json --replication-task-settings file://replication-task-settings.json
